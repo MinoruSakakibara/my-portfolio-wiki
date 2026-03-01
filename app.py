067e80ac70ca800d0c7dcda71433f9d439194391
Minoru Sakakibara <qqgm7z69@gmail.com>
2026-03-01 21:17:01 +0900

    app.py update

--- app.py
+++ app.py
@@ -5,9 +5,11 @@ from models import db, Article
 app = Flask(__name__)
 
 # データベース設定
-basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'test.db')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+database_url = os.environ.get('DATABASE_URL')
+if database_url and database_url.startswith("postgres://"):
+    database_url = database_url.replace("postgres://", "postgresql://", 1)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = database_url or 'sqlite:///test.db'
 
 db.init_app(app)
 
